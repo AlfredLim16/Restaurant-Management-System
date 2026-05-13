859d009eda9ff24233b6cbb5c1c830fac8dc7cbc
Alfred Lim <alfredmanelim@gmail.com>
2026-05-13 10:40:43 +0800

    Initial commit

---
+++ src/main/java/restaurantmanagementsystem/RestaurantManagementSystem.java
@@ -0,0 +1,13 @@
+
+package restaurantmanagementsystem;
+
+/**
+ *
+ * @author admin
+ */
+public class RestaurantManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
